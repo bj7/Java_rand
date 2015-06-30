ed8635da1e81ae5e1830cf3fc243e57a825008dc
Joshua Bernitt <jjbernitt@gmail.com>
2015-06-29 21:52:41 -0400

    First build out of classes
    Building out initial class and information.

--- src/com/rand/Main.java
+++ src/com/rand/Main.java
@@ -3,6 +3,8 @@ package com.rand;
 public class Main {
 
     public static void main(String[] args) {
+        Rand_num num = new Rand_num();
 
+		System.out.println(num.getValue());
     }
 }
--- src/com/rand/Rand_num.java
+++ src/com/rand/Rand_num.java
@@ -1,7 +1,17 @@
 package com.rand;
-
 /**
  * Created by joshuabernitt on 6/29/15.
  */
 public class Rand_num {
+	private long value;
+
+	public Rand_num() {
+		this.value = 0;
+	}
+
+	public long getValue() {
+		return this.value;
+	}
+
+
 }
