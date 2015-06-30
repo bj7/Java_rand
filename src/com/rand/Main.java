8d4d8bfc6c4f514e97e7b086f33e0d3bac39251c
Joshua Bernitt <jjbernitt@gmail.com>
2015-06-29 22:20:10 -0400

    Added utilities package
    Utilities package for useful code snippets.

--- src/com/rand/Main.java
+++ src/com/rand/Main.java
@@ -1,10 +1,11 @@
 package com.rand;
 
-public class Main {
+import utilities.utilities;
 
+public class Main {
     public static void main(String[] args) {
         Rand_num num = new Rand_num();
 
-		System.out.println(num.getValue());
+		utilities.print(num.getValue());
     }
 }
---
+++ src/utilities/utilities.java
@@ -0,0 +1,11 @@
+package utilities;
+
+/**
+ * Created by joshuabernitt on 6/29/15.
+ */
+public class utilities {
+	public static <PrintToString> void print(PrintToString args) {
+		System.out.print(args);
+		System.out.println();
+	}
+}
